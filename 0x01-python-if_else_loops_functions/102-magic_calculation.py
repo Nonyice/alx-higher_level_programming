6e8168fe169f0f5b13c9ab88a05e7f0f2cd73938
Emmanuel <emmanuelcibe@yahoo.com>
2022-11-29 13:11:47 -0800

    Magic cal

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # Author - Emmanuel Ibe
 
-    def magic_calculation(a, b, c):
+def magic_calculation(a, b, c):
 
-    """Match bytecode provided by Holberton School."""
+"""Match bytecode provided by Holberton School."""
     
     if a < b:
-    return (c)
+return (c)
     if c > b:
-    return (a + b)
-    return (a*b - c)
+return (a + b)
+return (a*b - c)
